bcd4136a6ac733bf6b85235ebe51708701748992
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2023-05-11 23:19:44 +0600

    110A

--- 110A-Nearly_Lucky_Number.c
+++ 110A-Nearly_Lucky_Number.c
@@ -17,15 +17,13 @@ int main()
             n = n / 10;
         }
 
-        // printf("%d number of lucky digits\n", len);
-
         if (len == 4 || len == 7)
         {
             printf("YES\n");
         }
         else
         {
-            // printf("NO\n");
+            printf("NO\n");
         }
     }
     else
