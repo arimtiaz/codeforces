c2e07a3c8caa3d155887e90721ed4324f04d9c3c
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2023-05-15 17:46:17 +0600

    1030A

--- 1030A-In_Search_of_an_Easy_Problem.c
+++ 1030A-In_Search_of_an_Easy_Problem.c
@@ -4,7 +4,7 @@ int main()
 {
     long long int n;
     scanf("%lld", &n);
-    int arr[n], count1 = 0, count2 = 0;
+    int arr[n], count = 0;
 
     for (int i = 0; i < n; i++)
     {
@@ -13,13 +13,12 @@ int main()
 
     for(int i = 0; i<n; i++){
         if(arr[i] >= 1){
-            count1++;
-        } else{
-            count2++;
+            count++;
         }
     }
+    
 
-    if(count1 >= 1){
+    if(count >= 1){
         printf("HARD\n");
     } else{
         printf("EASY\n");
